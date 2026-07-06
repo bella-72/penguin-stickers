753458ac627fd9b30db586459c2c487b30602ce5
bamala <mbasmla76@gmail.com>
2026-07-06 14:20:05 +0300

    update photos

--- scripts/sync-products.mjs
+++ scripts/sync-products.mjs
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import fs from "fs-extra";
 import path from "path";
 import mime from "mime-types";
-
+import slugify from "slugify";
 dotenv.config();
 
 const supabase = createClient(
@@ -12,8 +12,38 @@ const supabase = createClient(
 );
 
 const PRODUCTS_DIR = "./products";
+async function getOrCreateCategory(folderName) {
+  const slug = slugify(folderName, { lower: true });
 
+  // ندور هل الـ Category موجودة
+  const { data: existing } = await supabase
+    .from("categories")
+    .select("*")
+    .eq("slug", slug)
+    .maybeSingle();
+
+  if (existing) return existing;
+
+  // لو مش موجودة نعملها
+  const { data, error } = await supabase
+    .from("categories")
+    .insert({
+      name: folderName,
+      slug: slug,
+    })
+    .select()
+    .single();
+
+  if (error) throw error;
+
+  console.log(`📁 Created Category: ${folderName}`);
+
+  return data;
+}
 async function uploadFolder(folderName) {
+
+
+    const category = await getOrCreateCategory(folderName);
 
     const folderPath = path.join(PRODUCTS_DIR, folderName);
 
@@ -53,6 +83,65 @@ async function uploadFolder(folderName) {
         }
 
         console.log(`✅ ${file}`);
+        const { data: publicData } = supabase.storage
+  .from("products")
+  .getPublicUrl(storagePath);
+
+const publicUrl = publicData.publicUrl;
+
+const productName = path.parse(file).name;
+// ندور هل المنتج موجود
+const { data: existingProduct } = await supabase
+  .from("products")
+  .select("*")
+  .eq("name", productName)
+  .maybeSingle();
+
+if (existingProduct) {
+
+  // المنتج موجود → نحدث الصور فقط
+  const currentImages = existingProduct.images || [];
+
+  if (!currentImages.includes(publicUrl)) {
+
+    await supabase
+      .from("products")
+      .update({
+        images: [...currentImages, publicUrl]
+      })
+      .eq("id", existingProduct.id);
+
+    console.log(`📝 Updated Product: ${productName}`);
+  }
+
+} else {
+
+  // المنتج مش موجود → نعمله Insert
+  const slug = slugify(productName, { lower: true });
+
+  const { error: insertError } = await supabase
+    .from("products")
+    .insert({
+      name: productName,
+      slug: slug,
+      category_id: category.id,
+      price: 7,
+      stock: 999,
+      rating: 5,
+      review_count: 0,
+      is_featured: false,
+      is_new: true,
+      finish_types: ["matte"],
+      images: [publicUrl]
+    });
+
+  if (insertError) {
+    console.log(insertError.message);
+  } else {
+    console.log(`➕ Added Product: ${productName}`);
+  }
+
+}
     }
 }
 
