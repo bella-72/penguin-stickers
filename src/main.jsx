d34035b6fdf5c50564974a90e231d8d5dd492669
bamala <mbasmla76@gmail.com>
2026-07-23 13:29:06 +0300

    kokokokoko

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ReactPixel from 'react-facebook-pixel'
+
+// Initialize Meta Pixel
+ReactPixel.init('1230623762470783')
+ReactPixel.pageView()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
